922508f797b95d37d64895d0a2f63341002316ab
Gayan R <gayanramyakumara@gmail.com>
2025-03-22 09:58:30 +0530

    update mysql driver

--- new1/new1/settings.py
+++ new1/new1/settings.py
@@ -75,13 +75,26 @@ WSGI_APPLICATION = 'new1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',  # Change from sqlite3 to mysql
+        'NAME': 'django_api',  # Set your database name
+        'USER': 'phpmyadmin',  # Your MySQL username
+        'PASSWORD': '1234',  # Your MySQL password
+        'HOST': 'localhost',  # If MySQL is running locally
+        'PORT': '3306',  # Default MySQL port
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
