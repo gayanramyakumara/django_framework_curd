4855d4ef903f6acac3dda06acf95788d91fcf19a
Gayan R <gayanramyakumara@gmail.com>
2025-03-22 09:35:39 +0530

    refactoring the code

--- new1/api/migrations/0001_initial.py
+++ new1/api/migrations/0001_initial.py
@@ -16,10 +16,11 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=100)),
+                ('age', models.CharField(max_length=100)),
                 ('email', models.EmailField(max_length=254)),
                 ('password', models.CharField(max_length=100)),
                 ('phone', models.CharField(max_length=100)),
-                ('address', models.TextField()),
+                ('address', models.TextField(max_length=100)),
             ],
         ),
     ]
--- new1/api/models.py
+++ new1/api/models.py
@@ -4,6 +4,10 @@ from django.db import models
 class User(models.Model):
     age = models.IntegerField()
     name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    email = models.EmailField(max_length=254)
+    password = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
